fb0cade312a9d7bd1cbe64413911adaf7c2921cd
gustavvz <gustav.vonzitzewitz@bshg.com>
2017-12-21 16:12:59 +0100

    first commit

---
+++ object_detection.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 21 12:01:40 2017
+
+@author: gustav
+"""
+# Default Google ipynb tutorial
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+import cv2
+
+# Add Libraries to PYTHONPATH
+#sys.path.append('../models/research')
+#sys.path.append('../models/research/slim')
+#sys.path.append('../object_detection')
+
+# Protobuf Compilation
+#os.system('protoc object_detection/protos/*.proto --python_out=.')
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+# Define Video Input
+# 0 = Default Camera
+video_input = 0
+width = 480
+height = 360
+
+#Current working path
+CWD_PATH = os.getcwd()
+
+# Model preparation
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+# List of the strings that is used to add correct label for each box.
+LABEL_MAP = 'mscoco_label_map.pbtxt'
+PATH_TO_LABELS = 'object_detection/data/' + LABEL_MAP
+NUM_CLASSES = 90
+
+# Download Model    
+if not os.path.isfile(PATH_TO_CKPT):
+    print('Model not found. Downloading it now.')
+    opener = urllib.request.URLopener()
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+    tar_file = tarfile.open(MODEL_FILE)
+    for file in tar_file.getmembers():
+      file_name = os.path.basename(file.name)
+      if 'frozen_inference_graph.pb' in file_name:
+        tar_file.extract(file, os.getcwd())
+    os.remove('../' + MODEL_FILE)
+else:
+    print('Model found. Proceed.')
+
+# Load a (frozen) Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+# Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+print ("Press 'q' to Exit")
+
+video_stream = cv2.VideoCapture(video_input)
+video_stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+video_stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+# Detection
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess: # config=tf.ConfigProto(log_device_placement=True)
+    # Definite input and output Tensors for detection_graph
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    
+    while video_stream.isOpened():
+      ret_val,image_np = video_stream.read()
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      # Actual detection.
+      (boxes, scores, classes, num) = sess.run(
+          [detection_boxes, detection_scores, detection_classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      cv2.imshow('Visualizer', image_np)
+      # Exit Option
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+      
+cv2.destroyAllWindows()
+
